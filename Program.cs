5c1012426b2ebda0a1fb7c213a084d500c26378c
tatojastine-n <jajanicoletato@gmail.com>
2025-08-07 13:15:51 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_Registration_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> courses = new List<string>();
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write($"Enter course code {i + 1}: ");
+                
+                string input = Console.ReadLine().Trim();
+
+                if (input.Length != 5)
+                {
+                    Console.WriteLine($"Error: Course code must be 5 characters long.");
+                    continue;
+                }
+                courses.Add(input);
+                
+                Console.WriteLine($"Successfully added course: {input}");
+            }
+            
+            Console.WriteLine("\nValid Courses:");
+            
+            foreach (string course in courses)
+            {
+                Console.WriteLine(course);
+            }
+        }
+    }
+}
